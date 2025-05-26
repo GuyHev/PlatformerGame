0d814a8e95e62a642743663a8f364471d0dc3e5c
GuyHev <guy1guy21@gmail.com>
2025-05-26 15:03:15 +0300

    started new modifed branch

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,71 +1,118 @@
 extends CharacterBody2D 
 
-
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-const GRAVITY = 900
 const KNOCKBACK_DURATION = 0.4
 
 @onready var sprite = $AnimatedSprite2D
+
+var profiles = {
+	"normal": {"speed": 130.0, "jump_velocity": -300.0,"gravity": 900.0},
+	"water": {"speed": 80.0, "jump_velocity": -180.0,"gravity": 0.0}
+}
+var current_profile = "normal"
+var is_in_water = false
+# Physics values
+var speed = 130.0
+var jump_velocity = -300.0
+var gravity = 900
 
 var knockback_velocity := Vector2.ZERO
 var knockback_timer := 0.0
 
 func _ready() -> void:
 	Signals.on_hit.connect(self.knockback)
+
 func _physics_process(delta: float) -> void:
-	if not is_dead():
-		if not((Global.life <= 0)):
-			pass
+	if is_dead():
+		die()
+	else:
 		if knockback_timer > 0:
 			knockback_timer -= delta
 			velocity = knockback_velocity
 		else:
 			var direction := Input.get_axis("move_left", "move_right")
+			current_profile = "water" if is_in_water else "normal"
+			# Load values from dictionary
+			var value_set = profiles[current_profile]
+			speed = value_set["speed"]
+			jump_velocity = value_set["jump_velocity"]
+			gravity = value_set["gravity"]
 			handle_physics(delta, direction)
 			handle_animation(direction)
 		move_and_slide()
-	else:
-		die()
+
 func handle_physics(delta: float, direction: float) -> void:
-	# Apply gravity
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta	
-		
-	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	# Horizontal input
-	if direction != 0:
-		velocity.x = direction * SPEED
-		sprite.flip_h = direction < 0 #the right part afte '=' return boolean expression
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-func handle_animation(direction: float) -> void:
-	#check if player in air
-	if not is_on_floor():
-		if velocity.y < 0:
-			sprite.play("jump")  # Going up
+	if current_profile == "water":
+		if direction != 0:
+			velocity.x = direction * speed
+			sprite.flip_h = direction < 0 #the right part afte '=' return boolean expression
 		else:
-			sprite.play("fall")  # Coming down
-	#check if player in not standing still
-	elif direction != 0:
-		sprite.play("run")
+			velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = 0.0
+		if Input.is_action_pressed("move_up"):
+			velocity.y = -speed
+		elif Input.is_action_pressed("move_down"):
+			velocity.y = speed
+			
+		velocity.x = direction * speed
+	if current_profile == "normal":
+		# Apply gravity
+		if not is_on_floor():
+			velocity.y += gravity * delta	
+			
+		# Handle jump
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_velocity
+			
+		# Horizontal input
+		if direction != 0:
+			velocity.x = direction * speed
+			sprite.flip_h = direction < 0 #the right part afte '=' return boolean expression
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+
+func handle_animation(direction: float) -> void:
+	if current_profile == "water":
+		sprite.play("swim")
 	else:
-		sprite.play("idle")
+		#check if player in air
+		if not is_on_floor():
+			if velocity.y < 0:
+				sprite.play("jump")  # Going up
+			else:
+				sprite.play("fall")  # Coming down
+		#check if player in not standing still
+		elif direction != 0:
+			sprite.play("run")
+		else:
+			sprite.play("idle")
+
 func bounce(force: float) -> void:
+	
 	velocity.y = force
+
 func knockback(from_position: Vector2 = global_position, strength: float = 200.0) -> void:
 	var direction = (global_position - from_position).normalized()
 	knockback_velocity = direction * strength
 	knockback_timer = KNOCKBACK_DURATION
 	sprite.play("hit")
+
 func die() -> void:
 	get_tree().reload_current_scene()
 	Global.life = 3
 	Global.picked_pineapples = 0
+
 func is_dead() -> bool:
 	if	(Global.life <= 0):
 		return	true
-	return false		
+	return false
+
+#detect specific layers which the player has entered
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	is_in_water = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_in_water = false
+
+	# Give a small upward push when leaving water
+	if velocity.y >= 0:
+		velocity.y = 300  # tweak as needed
