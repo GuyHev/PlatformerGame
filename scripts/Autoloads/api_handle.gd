49a2b362e918204b3495f423db5cf845fd922f32
GuyHev <guy1guy21@gmail.com>
2025-07-08 22:51:26 +0300

    added leaderboard in the end scene

--- scripts/Autoloads/api_handle.gd
+++ scripts/Autoloads/api_handle.gd
@@ -3,6 +3,7 @@ class_name API
 
 var key
 var public_key
+signal top_ten_ready(xml_data: PackedByteArray)
 
 func _ready() -> void:
 	var config = ConfigFile.new()
@@ -10,11 +11,14 @@ func _ready() -> void:
 	if err == OK:
 		key = config.get_value("keys", "key")
 		public_key = config.get_value("keys", "public_key")
+		
+#func get_top_10() -> PackedByteArray:
+	#return xml
 
 func upload_score(player_name: String, score: int) -> void:
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.request_completed.connect(self._http_request_completed)
+	http_request.request_completed.connect(self._http_request_completed.bind(0))
 
 	var url = key + "add/%s/%d" % [player_name, score]
 	print("Requesting URL:", url)
@@ -23,10 +27,10 @@ func upload_score(player_name: String, score: int) -> void:
 	if error != OK:
 		push_error("An error occurred in the HTTP request: %s" % error)
 
-func get_data() -> void:
+func fetch_data() -> void:
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.request_completed.connect(self._http_request_completed)
+	http_request.request_completed.connect(self._http_request_completed.bind(0))
 	
 	var url = public_key + "pipe"
 	print("Requesting URL:", url)
@@ -38,7 +42,7 @@ func get_data() -> void:
 func clear_data() -> void:
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.request_completed.connect(self._http_request_completed)
+	http_request.request_completed.connect(self._http_request_completed.bind(0))
 
 	var url = key + "clear"
 	print("Requesting URL:", url)
@@ -46,10 +50,30 @@ func clear_data() -> void:
 	var error = http_request.request(url)
 	if error != OK:
 		push_error("An error occurred in the HTTP request: %s" % error)
+		
+func fetch_top_ten() -> void:
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._http_request_completed.bind(1))
+	var url = public_key + "xml/10"
+	print("Requesting URL:", url)
 
-func _http_request_completed(result, response_code, headers, body):
+	var error = http_request.request(url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request: %s" % error)
+
+func _http_request_completed(result, response_code, headers, body, request_type):
 	print("HTTP Request Completed")
 	print("Result:", result)
 	print("Response Code:", response_code)
+	
+	if result != OK or response_code != 200:
+		print("Request failed.")
+		return
+		
 	print("Headers:", headers)
 	print("Body:", body.get_string_from_utf8())
+	
+	if request_type == 1:
+		var xml = body
+		emit_signal("top_ten_ready", xml)
---
+++ scripts/UI/end_game/leaderboard.gd
@@ -0,0 +1,72 @@
+extends GridContainer
+
+const OFFSET = 2 # Number of children to skip in GridContainer
+
+var names = []
+var scores = []
+
+func _ready() -> void:
+	api.fetch_top_ten()
+	api.top_ten_ready.connect(set_labels)
+
+	
+func set_labels(xml_bytes : PackedByteArray) -> void:	
+	var children = self.get_children() 
+	fill_arrays(xml_bytes)
+	for i in range(names.size()):
+		var score_label = children[OFFSET + (i * 2 + 1)] # odd index
+		var name_label = children[OFFSET + (i * 2)] # even index
+
+		if score_label is Label:
+			score_label.text = str(scores[i])
+		if name_label is Label:
+			name_label.text = names[i]
+
+func fill_arrays(xml_bytes : PackedByteArray) -> void:
+	var parser = XMLParser.new()
+	var err = parser.open_buffer(xml_bytes)
+	if err != OK:
+		push_error("Failed to open XML buffer")
+		return
+
+	names.clear() 
+	scores.clear()
+	var inside_entry = false
+	var current_tag = ""
+	var current_name = ""
+	var current_score = 0
+
+	while true:
+		var status = parser.read()
+		if status == ERR_FILE_EOF:
+			break
+		elif status != OK:
+			push_error("XML parsing error: " + str(parser.get_error_message()))
+			return
+
+		match parser.get_node_type():
+			XMLParser.NODE_ELEMENT:
+				current_tag = parser.get_node_name()
+				if current_tag == "entry":
+					inside_entry = true
+					current_name = ""
+					current_score = 0
+
+			XMLParser.NODE_TEXT:
+				if inside_entry:
+					var text = parser.get_node_data().strip_edges()
+					if current_tag == "name":
+						current_name = text
+					elif current_tag == "score":
+						current_score = int(text)
+
+			XMLParser.NODE_ELEMENT_END:
+				if parser.get_node_name() == "entry":
+					# Finished one entry, add to arrays
+					inside_entry = false
+					names.append(current_name)
+					scores.append(current_score)
+
+	# Now you have two arrays with the top scores data
+	print("Names:", names)
+	print("Scores:", scores)
