05eda60c4251c22c7823202e179a668d7df5fde4
GuyHev <guy1guy21@gmail.com>
2025-07-30 23:51:41 +0300

    changed api

--- scripts/UI/end_game/leaderboard.gd
+++ scripts/UI/end_game/leaderboard.gd
@@ -1,72 +1,27 @@
 extends GridContainer
 
-const OFFSET = 2 # Number of children to skip in GridContainer
-
-var names = []
-var scores = []
+const OFFSET = 2
+@export var leaderboard_name: String = "leaderboard"
 
 func _ready() -> void:
-	api.top_ten_ready.connect(set_labels)
-	api.upload_score_done.connect(on_upload_done)
-	api.fetch_top_ten()
-	
-func on_upload_done() -> void:
-	print("Upload done, fetching new leaderboard...")
-	api.fetch_top_ten()
+	await Talo.leaderboards.get_entries(leaderboard_name)
+	set_labels()
 
-func set_labels(xml_bytes : PackedByteArray) -> void:	
-	var children = self.get_children() 
-	fill_arrays(xml_bytes)
-	for i in range(names.size()):
-		var score_label = children[OFFSET + (i * 2 + 1)] # odd index
-		var name_label = children[OFFSET + (i * 2)] # even index
+func set_labels() -> void:
+	var count = 0
+	var children = get_children()
+	var entries = Talo.leaderboards.get_cached_entries(leaderboard_name)
+
+	for i in range(min(entries.size(), (children.size() - OFFSET) / 2)):
+		if count == 20:
+			return
+		var entry = entries[i]
+		var name_label = children[OFFSET + (i * 2)]
+		var score_label = children[OFFSET + (i * 2 + 1)]
+
+		if name_label is Label:
+			name_label.text = entry.player_alias.identifier
 
 		if score_label is Label:
-			score_label.text = str(scores[i])
-		if name_label is Label:
-			name_label.text = names[i]
-
-func fill_arrays(xml_bytes : PackedByteArray) -> void:
-	var parser = XMLParser.new()
-	var err = parser.open_buffer(xml_bytes)
-	if err != OK:
-		push_error("Failed to open XML buffer")
-		return
-
-	names.clear() 
-	scores.clear()
-	var inside_entry = false
-	var current_tag = ""
-	var current_name = ""
-	var current_score = 0
-
-	while true:
-		var status = parser.read()
-		if status == ERR_FILE_EOF:
-			break
-		elif status != OK:
-			push_error("XML parsing error: " + str(parser.get_error_message()))
-			return
-
-		match parser.get_node_type():
-			XMLParser.NODE_ELEMENT:
-				current_tag = parser.get_node_name()
-				if current_tag == "entry":
-					inside_entry = true
-					current_name = ""
-					current_score = 0
-
-			XMLParser.NODE_TEXT:
-				if inside_entry:
-					var text = parser.get_node_data().strip_edges()
-					if current_tag == "name":
-						current_name = text
-					elif current_tag == "score":
-						current_score = int(text)
-
-			XMLParser.NODE_ELEMENT_END:
-				if parser.get_node_name() == "entry":
-					# Finished one entry, add to arrays
-					inside_entry = false
-					names.append(current_name)
-					scores.append(current_score)
+			score_label.text = str(entry.score)
+		count += 1
